62eab2561286e96035f580a57680105b9f10d4d7
BlueKrow <ndioses@gmail.com>
2020-02-16 12:50:33 +0100

    fix: find out how sut works

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-set -e
+#set -e
 
 if [ "$1" = "check" ]; then
     echo "Container is ready to be used"
     exit 0
 fi
 
-echo "Executing override command"
-exec "$@"
+#echo "Executing override command"
+#exec "$@"
