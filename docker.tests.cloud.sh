c98647182c4868a686ae12e5c8d7c9f85c76aebe
BlueKrow <ndioses@gmail.com>
2020-02-16 17:38:43 +0100

    feat: add ruby installation

--- docker.tests.cloud.sh
+++ docker.tests.cloud.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#Check Ubuntu Installation
+#Check Ubuntu and Software Installation
 NIX_OS="ubuntu"
 NIX_DIST="eoan"
+RUBY_INSTALL="ruby"
 
 echo
 echo ------------------
@@ -20,12 +21,23 @@ fi
 
 echo
 RESULT_DIST=$(cat /etc/os-release | grep '^VERSION_CODENAME=' | sed -e 's/^VERSION_CODENAME=//' )
-echo Expected DIST: $RESULT_DIST
-echo Actual DIST: $NIX_DIST
+echo Expected DIST: $NIX_DIST
+echo Actual DIST: $RESULT_DIST
 if [ "$RESULT_DIST" = "$NIX_DIST" ]; then 
     echo "Test - Installed Distribution: SUCCESS"
 else
     echo "Test - Installed Distribution: FAILED"
+    exit 1
+fi
+
+echo
+RESULT_RUBY=$(ruby --version | sed -e 's/\ .*//')
+echo Expected Software: $RUBY_INSTALL
+echo Actual Software: $RESULT_RUBY
+if [ "$RESULT_RUBY" = "$RUBY_INSTALL" ]; then 
+    echo "Test - Ruby Installation: SUCCESS"
+else
+    echo "Test - Ruby Installation: FAILED"
     exit 1
 fi
 
