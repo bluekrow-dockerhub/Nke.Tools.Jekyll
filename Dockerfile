d61a0d705150ce7f56c5bf19ad0184c91cd23571
BlueKrow <ndioses@gmail.com>
2020-02-16 19:45:31 +0100

    feat: add ruby gems installation

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM ubuntu:eoan
 LABEL mantainer=BlueKrow
+ENV GEM_HOME="/usr/local/bundle"
+ENV PATH $GEM_HOME/bin:$GEM_HOME/gems/bin:$PATH
+
 COPY ./entrypoint.sh /
 RUN apt-get update -y \
- && apt-get install ruby -y
+ && apt-get install ruby-full build-essential zlib1g-dev -y 
+
 ENTRYPOINT ["/entrypoint.sh"]
 CMD ["check"]
--- docker.tests.cloud.sh
+++ docker.tests.cloud.sh
@@ -1,43 +1,76 @@
 #!/bin/sh
 
-#Check Ubuntu and Software Installation
-NIX_OS="ubuntu"
-NIX_DIST="eoan"
-RUBY_INSTALL="ruby"
+echoInfo () {
+	echo "\e[92m$1\e[0m"
+}
+
+echoError () {
+	echo "\e[91m\e[5m$1\e[0m"
+}
 
 echo
 echo ------------------
 echo INITIALIZE TESTING
 echo ------------------
+#Check Ubuntu installation
+NIX_OS="ubuntu"
 RESULT_OS=$(cat /etc/os-release | grep '^ID=' | sed -e 's/^ID=//' )
 echo Expected OS: $NIX_OS
 echo Actual OS: $RESULT_OS
 if [ "$RESULT_OS" = "$NIX_OS" ]; then 
-    echo "Test - Installed OS: SUCCESS"
+    echoInfo "Test - Installed OS: SUCCESS"
 else
-    echo "Test - Installed OS: FAILED"
+    echoError "Test - Installed OS: FAILED"
     exit 1
 fi
 
 echo
+#Check Distro installation
+NIX_DIST="eoan"
 RESULT_DIST=$(cat /etc/os-release | grep '^VERSION_CODENAME=' | sed -e 's/^VERSION_CODENAME=//' )
 echo Expected DIST: $NIX_DIST
 echo Actual DIST: $RESULT_DIST
 if [ "$RESULT_DIST" = "$NIX_DIST" ]; then 
-    echo "Test - Installed Distribution: SUCCESS"
+    echoInfo "Test - Installed Distribution: SUCCESS"
 else
-    echo "Test - Installed Distribution: FAILED"
+    echoError "Test - Installed Distribution: FAILED"
     exit 1
 fi
 
 echo
+#Check Ruby installation
+RUBY_INSTALL="ruby"
 RESULT_RUBY=$(ruby --version | sed -e 's/\ .*//')
 echo Expected Software: $RUBY_INSTALL
 echo Actual Software: $RESULT_RUBY
 if [ "$RESULT_RUBY" = "$RUBY_INSTALL" ]; then 
-    echo "Test - Ruby Installation: SUCCESS"
+    echoInfo "Test - Ruby Installation: SUCCESS"
 else
-    echo "Test - Ruby Installation: FAILED"
+    echoError "Test - Ruby Installation: FAILED"
+    exit 1
+fi
+
+echo
+#Check Gems installation
+RESULT_GEMS=$(gem --version)
+echo Actual Software: $RESULT_GEMS
+if [ -n "$RESULT_GEMS" ]; then 
+    echoInfo "Test - RubyGems  Installation: SUCCESS"
+else
+    echoError "Test - RubyGems Installation: FAILED"
+    exit 1
+fi
+
+echo
+#Check Gems Home installation
+GEM_HOME_INSTALL="/usr/local/bundle"
+RESULT_GEM_HOME=$GEM_HOME
+echo Expected Software: $GEM_HOME_INSTALL
+echo Actual Software: $RESULT_GEM_HOME
+if [ "$RESULT_GEM_HOME" = "$GEM_HOME_INSTALL" ]; then 
+    echoInfo "Test - RubyGems Home Installation: SUCCESS"
+else
+    echoError "Test - RubyGems Home Installation: FAILED"
     exit 1
 fi
 
