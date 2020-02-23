33ea1badb85e519170c6fd9609d7e7adfc35984d
BlueKrow <ndioses@gmail.com>
2020-02-24 00:02:09 +0100

    feat: add jekyll execution options

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,23 @@ if [ "$1" = "bash" ]; then
     exit 0
 fi
 
+if [ "$1" = "jkl-new" ]; then
+    bundle exec jekyll new .
+    exit 0
+fi
+
+if [ "$1" = "jkl-build" ]; then
+    bundle install --path=/jkl-site/.gems-cache
+    bundle exec jekyll build
+    exit 0
+fi
+
+if [ "$1" = "jkl-serve" ]; then
+    bundle install --path=/jkl-site/.gems-cache
+    bundle exec jekyll serve --host 0.0.0.0
+    exit 0
+fi
+
 if [ "$#" -eq 0 ]; then
     echo "No parameters supplied. Execute Default Action"
     exit 0
